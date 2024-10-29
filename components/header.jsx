149648c51bf68f173de80ee9e1af7fd6afb5e761
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2024-10-29 09:47:47 +0800

    Add website header

---
+++ components/header.jsx
@@ -0,0 +1,65 @@
+import HeaderNavLink from './header_navlink';
+import { Button } from './ui/button';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { createClient } from '@/utils/supabase/server';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default async function Header() {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const isLoggedIn = user !== null;
+  const isAdmin = false;
+
+  return (
+    <header className="sticky top-0 flex items-center gap-8 bg-black px-4 py-2.5 drop-shadow-md">
+      <div className="flex items-center gap-3 text-white">
+        <Image src="/logo.png" width={50} height={50} alt="MoRooms Logo" />
+        <h1 className="select-none text-2xl font-bold">MoRooms</h1>
+      </div>
+
+      <nav className="flex-1">
+        <HeaderNavLink link="/">Home</HeaderNavLink>
+        <HeaderNavLink link="/todo">Profile</HeaderNavLink>
+        <HeaderNavLink link="/todo">Reservations</HeaderNavLink>
+
+        {isAdmin && (
+          <>
+            <HeaderNavLink link="/todo">Manage Rooms</HeaderNavLink>
+            <HeaderNavLink link="/todo">Manage Reservations</HeaderNavLink>
+            <HeaderNavLink link="/todo">Manage Users</HeaderNavLink>
+          </>
+        )}
+      </nav>
+
+      <div className="flex gap-3">
+        {isLoggedIn ? (
+          <Link href="/profile">
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </Link>
+        ) : (
+          <>
+            <Button
+              asChild
+              className="hover:brightness-110 active:brightness-75"
+            >
+              <Link href="/login">Login</Link>
+            </Button>
+            <Button
+              asChild
+              className="border border-white hover:brightness-110 active:brightness-75"
+            >
+              <Link href="/signup">Sign Up</Link>
+            </Button>
+          </>
+        )}
+      </div>
+    </header>
+  );
+}
