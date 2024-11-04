d41c64dd75e024dc3e758caa29010ec79e81a160
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2024-11-04 20:57:54 +0800

    Add add room type pop-up form

--- components/manage/room_types/add_room_type_button.jsx
+++ components/manage/room_types/add_room_type_button.jsx
@@ -1,8 +1,39 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
-import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Plus } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const FORM_SCHEMA = z.object({
+  name: z.string().min(2, {
+    message: 'Type name must be at least 2 characters.',
+  }),
+  details: z.string().optional(),
+});
 
 export default function AddRoomTypeButton() {
+  const form = useForm({
+    resolver: zodResolver(FORM_SCHEMA),
+  });
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -12,7 +43,50 @@ export default function AddRoomTypeButton() {
         </Button>
       </DialogTrigger>
 
-      <DialogContent>{/* <RoomTypeForm /> */}</DialogContent>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add Room Type</DialogTitle>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={form.handleSubmit(() => {})}
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="font-bold">Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Type" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="details"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="font-bold">Details</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button className="mt-4" type="submit">
+              Add Room Type
+            </Button>
+          </form>
+        </Form>
+      </DialogContent>
     </Dialog>
   );
 }
