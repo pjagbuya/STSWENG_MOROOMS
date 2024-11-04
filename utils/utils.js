76a5ba807886167a5e5d6ed2027edfbfbb00558c
Clydie-Juls <clyde_marindo@dlsu.edu.ph>
2024-11-05 02:23:53 +0800

    add admin ui profile

--- utils/utils.js
+++ utils/utils.js
@@ -1,2 +1,21 @@
+import { createClient as createClientServerSide } from './supabase/server';
+
 export const convertToNull = value =>
   value === undefined || value === '' ? null : value;
+
+export async function IsAdminServerSide() {
+  const supabase = createClientServerSide();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const { data, error } = await supabase.rpc('get_user_role', {
+    p_user_id: user.id,
+  });
+
+  if (error) {
+    throw new Error(error);
+  }
+
+  return data.toLowerCase() == 'admin';
+}
