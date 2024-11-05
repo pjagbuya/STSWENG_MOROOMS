8aebff85b46293e30b55cb800a1ac396d206e7d5
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2024-11-05 23:54:58 +0800

    Add CRUD functionality for room sets

--- components/manage/room_sets/room_set_table.jsx
+++ components/manage/room_sets/room_set_table.jsx
@@ -1,20 +1,96 @@
 'use client';
 
-import { DataTable, SortableHeader } from '@/components/data_table';
+import RoomSetDeletePopup from './room_set_delete_popup';
+import RoomSetEditPopup from './room_set_edit_popup';
+import {
+  deleteRoomSetAction,
+  editRoomSetAction,
+} from '@/app/manage/room_sets/actions';
+import { addActionColumn } from '@/components/util/action_dropdown';
+import { DataTable } from '@/components/util/data_table';
+import { SortableHeader } from '@/components/util/sortable_header';
+import { useState } from 'react';
 
 const COLUMNS = [
   {
-    accessorKey: 'event_name',
+    accessorKey: 'name',
     header: ({ column }) => (
       <SortableHeader column={column}>Name</SortableHeader>
     ),
   },
 ];
 
-export default function RoomSetTable() {
+export default function RoomSetTable({ data }) {
+  const [row, setRow] = useState(null);
+
+  const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+  const [openEditDialog, setOpenEditDialog] = useState(false);
+
+  const finalColumns = addActionColumn(
+    COLUMNS,
+    handleDeletePrompt,
+    handleEditPrompt,
+  );
+
+  function handleDeletePrompt(row) {
+    setRow(row);
+    setOpenDeleteDialog(true);
+  }
+
+  async function handleDeleteContinue() {
+    await deleteRoomSetAction(row.original.id);
+    setRow(null);
+  }
+
+  function handleEditPrompt(row) {
+    setRow(row);
+    setOpenEditDialog(true);
+  }
+
+  async function handleEditContinue(row, form, values) {
+    const err = await editRoomSetAction(row.original.id, values.name);
+
+    if (err) {
+      form.setError('event_name', err);
+      return;
+    }
+
+    setOpenEditDialog(false);
+    setRow(null);
+  }
+
   return (
     <>
-      <DataTable className="border-2" columns={COLUMNS} data={[]} />
+      <DataTable columns={finalColumns} data={data} />
+
+      <RoomSetDeletePopup
+        open={openDeleteDialog}
+        onCancel={() => {
+          setRow(null);
+          setOpenDeleteDialog(false);
+        }}
+        onDelete={handleDeleteContinue}
+        onOpenChange={v => {
+          setOpenDeleteDialog(v);
+
+          if (!v) {
+            setRow(null);
+          }
+        }}
+      />
+
+      <RoomSetEditPopup
+        row={row}
+        open={openEditDialog}
+        onEdit={handleEditContinue}
+        onOpenChange={v => {
+          setOpenEditDialog(v);
+
+          if (!v) {
+            setRow(null);
+          }
+        }}
+      />
     </>
   );
 }
