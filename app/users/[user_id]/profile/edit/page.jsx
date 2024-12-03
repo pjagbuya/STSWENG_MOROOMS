bb319d7d1d8d8cb4b5086072bc325666fe705732
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2024-12-04 02:10:52 +0800

    Fix profile page

--- app/users/[user_id]/profile/edit/page.jsx
+++ app/users/[user_id]/profile/edit/page.jsx
@@ -9,14 +9,12 @@ export default async function EditProfilePage() {
   return (
     <div>
       <Header />
-      <div className="flex h-screen w-full items-center justify-center px-4">
-        <Image
-          src="/images/Login-bg.png"
-          alt="DLSU classroom"
-          layout="fill"
-          className="absolute -z-10 opacity-50"
-        />
-        <Signup defaultValues={userInfo} isEdit={true} />
+      <div className="flex h-screen w-full items-center justify-center bg-opacity-25 bg-[url('/images/Login-bg.png')] bg-cover bg-no-repeat px-4">
+        <div className="fixed inset-0 bg-gray-300 opacity-70"></div>
+
+        <div className="relative z-20">
+          <Signup defaultValues={userInfo} isEdit={true} />
+        </div>
       </div>
     </div>
   );
