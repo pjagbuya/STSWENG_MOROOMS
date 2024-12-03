e522790eb0d5f8a8cd9b606ccc8c2cde72cce28f
maturuuu <matthew_kristoffer_ong@dlsu.edu.ph>
2024-12-03 17:26:48 +0800

    Update reservation-test.cy.js

--- cypress/e2e/reservation-test.cy.js
+++ cypress/e2e/reservation-test.cy.js
@@ -1,8 +1,8 @@
-describe('Profile page tests', () => {
+describe('Reservation / Manage Room tests', () => {
   const emailValid = 'test@gmail.com';
   const passwordValid = 'testuser';
-  const fname = 'Test';
-  const lname = 'User';
+  const searchTerm = "Joaquin's Room";
+  const searchRoom = "Joaquin's Room Test";
 
   beforeEach(() => {
     //establish a session so you don't need to keep logging in
@@ -18,25 +18,33 @@ describe('Profile page tests', () => {
 
     cy.visit('/');
     cy.contains('MoRooms').should('be.visible');
-    cy.contains('a', 'Profile').click();
+    cy.contains('a', 'Reservations / Manage Rooms').click();
+    cy.contains('Reserve a Room').should('be.visible');
   });
 
-  it('Profile has the correct name', () => {
-    cy.contains(`${fname} ${lname}`)
-      .should('be.visible')
-      .and('have.class', 'text-3xl')
-      .and('have.class', 'font-semibold');
+  it('Should display a grid with room cards', () => {
+    //checks for grid
+    cy.get('.grid').should('be.visible').and('have.css', 'display', 'grid');
+
+    //checks for cards (rooms)
+    cy.get('.grid div')
+      .should('have.length.greaterThan', 0)
+      .and('have.class', 'relative');
   });
 
-  it('Profile has the correct email', () => {
-    cy.contains(`${emailValid}`)
-      .should('be.visible')
-      .and('have.class', 'text-lg');
+  it('Can search a room', () => {
+    cy.get('input').type(searchTerm).should('have.value', searchTerm);
+    cy.get('button[type="submit"]').click();
+    cy.wait(1000);
+
+    cy.get('.grid > div:visible')
+      .should('have.length', 1)
+      .and('have.class', 'relative')
+      .and('contain.text', searchRoom);
+    cy.wait(500);
   });
 
-  it('Profile has the correct contact email', () => {
-    cy.contains(`Email: ${emailValid}`)
-      .should('be.visible')
-      .and('have.class', 'text-sm');
+  it('Can reserve a room', () => {
+    cy.get('a.block').find('article').find('div').contains(searchRoom).click();
   });
 });
