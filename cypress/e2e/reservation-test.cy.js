431e3c66b0c02cda834505dbe9342432e7f14621
maturuuu <matthew_kristoffer_ong@dlsu.edu.ph>
2024-12-04 02:17:01 +0800

    Update reservation-test.cy.js

--- cypress/e2e/reservation-test.cy.js
+++ cypress/e2e/reservation-test.cy.js
@@ -23,31 +23,31 @@ describe('Reservation / Manage Room tests', () => {
     cy.contains('MoRooms').should('be.visible');
   });
 
-  // it('Should display a grid with room cards', () => {
-  //   cy.contains('a', 'Reservations / Manage Rooms').click();
-  //   cy.contains('Reserve a Room').should('be.visible');
-  //   //checks for grid
-  //   cy.get('.grid').should('be.visible').and('have.css', 'display', 'grid');
+  it('Should display a grid with room cards', () => {
+    cy.contains('a', 'Reservations / Manage Rooms').click();
+    cy.contains('Reserve a Room').should('be.visible');
+    //checks for grid
+    cy.get('.grid').should('be.visible').and('have.css', 'display', 'grid');
 
-  //   //checks for cards (rooms)
-  //   cy.get('.grid div')
-  //     .should('have.length.greaterThan', 0)
-  //     .and('have.class', 'relative');
-  // });
+    //checks for cards (rooms)
+    cy.get('.grid div')
+      .should('have.length.greaterThan', 0)
+      .and('have.class', 'relative');
+  });
 
-  // it('Can search a room', () => {
-  //   cy.contains('a', 'Reservations / Manage Rooms').click();
-  //   cy.contains('Reserve a Room').should('be.visible');
-  //   cy.get('input').type(searchTerm).should('have.value', searchTerm);
-  //   cy.get('button[type="submit"]').click();
-  //   cy.wait(1000);
+  it('Can search a room', () => {
+    cy.contains('a', 'Reservations / Manage Rooms').click();
+    cy.contains('Reserve a Room').should('be.visible');
+    cy.get('input').type(searchTerm).should('have.value', searchTerm);
+    cy.get('button[type="submit"]').click();
+    cy.wait(1000);
 
-  //   cy.get('.grid > div:visible')
-  //     .should('have.length', 1)
-  //     .and('have.class', 'relative')
-  //     .and('contain.text', searchRoom);
-  //   cy.wait(500);
-  // });
+    cy.get('.grid > div:visible')
+      .should('have.length', 1)
+      .and('have.class', 'relative')
+      .and('contain.text', searchRoom);
+    cy.wait(500);
+  });
 
   it('Can reserve a room', () => {
     cy.contains('a', 'Reservations / Manage Rooms').click();
@@ -73,15 +73,8 @@ describe('Reservation / Manage Room tests', () => {
     cy.url().should('include', '/rooms');
   });
 
-  // it('The reserved room is visible', () => {
-  //   cy.contains('a', 'Home').click();
-  //   cy.wait(3000);
-  //   cy.contains(searchRoom).should('be.visible');
-  // });
-
-  //   it('The reservation can be deleted', () => {
-  //     cy.contains('a', 'Manage Reservations').click();
-  //     cy.contains(searchRoom).should('be.visible');
-  //     cy.contains(searchRoom).should('be.visible');
-  //   });
+  it('The reservation can be deleted', () => {
+    cy.contains('a', 'Home').click();
+    cy.contains('button', 'Delete').click();
+  });
 });
