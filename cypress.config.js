02e661a5b284732362ade0d5bb718ba6279ea866
pjagbuya <54502889+pjagbuya@users.noreply.github.com>
2025-09-05 22:50:39 +0800

    cypressconfigUpdates

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    experimentalSessionAndOrigin: true,
     defaultCommandTimeout: 30000,
     pageLoadTimeout: 30000,
   },
