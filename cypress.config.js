3c77cbb28cb00f5e28101e2c8c23ebd40b0fbfdf
pjagbuya <54502889+pjagbuya@users.noreply.github.com>
2025-09-05 23:11:54 +0800

    Token checks

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    experimentalSessionAndOrigin: true,
     defaultCommandTimeout: 30000,
     pageLoadTimeout: 30000,
   },
