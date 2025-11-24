8d0a2d0469ee80bb0bbf20135509eb00ca4f7d89
chaiestrella <elmeranita_estrella@dlsu.edu.ph>
2025-11-25 04:22:44 +0800

    Add files via upload

--- app/manage/room_sets/form_schema.js
+++ app/manage/room_sets/form_schema.js
@@ -3,5 +3,8 @@ import { z } from 'zod';
 export const FORM_SCHEMA = z.object({
   name: z.string().min(2, {
     message: 'Set name must be at least 2 characters.',
+  })
+  .max(50, {
+    message: 'Set name cannot exceed 50 characters.'
   }),
 });
